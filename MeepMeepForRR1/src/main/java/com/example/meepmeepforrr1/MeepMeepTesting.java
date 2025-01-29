25a122bb96fa5454316acd4e6f3091eb988b1499
silkysalt <106841743+silkysalt@users.noreply.github.com>
2025-01-29 17:52:07 -0500

    asd

--- MeepMeepForRR1/src/main/java/com/example/meepmeepforrr1/MeepMeepTesting.java
+++ MeepMeepForRR1/src/main/java/com/example/meepmeepforrr1/MeepMeepTesting.java
@@ -11,7 +11,7 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(5, 25, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(25, 25, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -73,18 +73,18 @@ public final class MecanumDrive {
         public double kA = 1;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 5;
+        public double maxWheelVel = 10;
         public double minProfileAccel = -15;
         public double maxProfileAccel = 25;
 
         // turn profile parameters (in radians)
-        public double maxAngVel = Math.PI; // shared with path
-        public double maxAngAccel = Math.PI;
+        public double maxAngVel = Math.PI/2; // shared with path
+        public double maxAngAccel = Math.PI/2;
 
         // path controller gains
-        public double axialGain = 1.5;
-        public double lateralGain = 2;
-        public double headingGain = 2; // shared with turn
+        public double axialGain = 1;
+        public double lateralGain = 1;
+        public double headingGain = 1; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
